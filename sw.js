6b287b8246f68466018ee664fff880ea5e9d4e2f
lorranemartinez-cpu <lorrane.martinez@gmail.com>
2026-07-02 11:17:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = "agenda-cm-v1";
+const ASSETS = ["/sala-reunioes/"];
+self.addEventListener("install", function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));
+});
+self.addEventListener("fetch", function(e){
+  e.respondWith(caches.match(e.request).then(function(r){
+    return r || fetch(e.request);
+  }));
+});
